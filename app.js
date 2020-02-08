37367d3b9b6a3edc3262e146644a7e909dce2c09
venum <mgautam@protonmail.com>
2020-02-08 18:15:53 +0100

    Update app.js
    Added documentation

--- app.js
+++ app.js
@@ -8,19 +8,50 @@ var app = express();
 app.use(bodyParser.urlencoded({extended: true })); 
 app.use(express.static('public'));
 
+/*
+Handles post requests for saving pastes
+*/
 app.post("/paste_publish", function(req, res) {
     content = req.body.paste_content;
-    var id = generateUID(6);
-    fs.appendFile('./pastes/' + id + ".paste", content, function (err) {
-        if (err) throw err;
-        console.log('File is created successfully.');
-        res.writeHead(302, {"Location" : "/p/" + id});
-        res.end();
+    var file;
+    var id;
+    while(!file || fs.existsSync(file)) {
+        id = generateUID(6);
+        file = './pastes/' + id + ".paste"
+    }
+    fs.appendFile(file, content, function (err) {
+        if (err) {
+            console.log("An error occured while creating/writing to paste file", err, file, content);
+            res.writeHead(500, {"Content-Type" : "text/plain"});
+            res.end("An unexpected server error occured while saving your paste. Sorry ¯\_(ツ)_/¯")
+        }else {
+            console.log("Paste " + id + " was created successfully. (at " + file + ")");
+            res.writeHead(301, {"Location" : "/p/" + id});
+            res.end();
+        }
     });
 });
 
+/*
+Inserts the text into the paste template
+*/
 app.get("/p/*", function(req, res) {
     var id = req.url.slice(3);
+    fs.readFile("./pastes/" + id + ".paste", function(error, content) {
+        if(error) {
+            console.log(error);
+            res.send("this paste does not seem to exist")
+        }else {
+            res.end(content);
+        }
+    })
+});
+
+/*
+Returns the raw text
+*/
+app.get("/raw/*", function(req, res) {
+    var id = req.url.slice(5);
     fs.readFile("./pastes/" + id + ".paste", function(error, content) {
         if(error) {
             res.send("this paste does not exist" + error)
@@ -30,6 +61,9 @@ app.get("/p/*", function(req, res) {
     })
 });
 
+/*
+Generates u
+*/
 const UIDCharacters = 'abcdefghijklmnopqrstuvwxyz';
 function generateUID(length) {
     var result = '';
