4322ad63867d1da05650772514842462642251ce
xrv0 <33809091+xrv0@users.noreply.github.com>
2020-02-22 21:12:16 +0100

    Updated stylesheet

--- public/main.js
+++ public/main.js
@@ -1,6 +1,5 @@
 const pasteTextarea = document.getElementById("paste_content");
 const pasteForm = document.getElementById("paste_form");
-const idLength = 3;
 
 function uploadPaste() {
     const content = pasteTextarea.value;
@@ -10,9 +9,8 @@ function uploadPaste() {
         console.log("Generating AES Key...");
         const key = generateKey(entropy);
         const encryptedContent = sjcl.encrypt(key, content);
-        const id = makeID(idLength);
-        pasteTextarea.value = id + "%" + encryptedContent;
-        localStorage.setItem(id, key);
+        pasteTextarea.value = encryptedContent;
+        localStorage.setItem("lastKey", key);
         pasteForm.submit();
     }
 }
@@ -24,16 +22,4 @@ function generateKey(entropy) {
     entropy = Math.ceil(entropy / 6) * 6; /* non-6-multiple produces same-length base64 */
     let key = sjcl.bitArray.clamp(sjcl.random.randomWords(Math.ceil(entropy / 32), 0), entropy);
     return sjcl.codec.base64.fromBits(key, 0).replace(/\=+$/, '').replace(/\//, '-');
-}
-
-/*
-Generates a pseudo random ID with given length
- */
-function makeID(length) {
-    let result = "";
-    const characters = `abcdefghijklmnopqrstuvwxyz123456789`;
-    for (let i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    return result;
 }
