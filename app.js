b13de300f4d31f2aaf14c09dccc4dc4e1ea625f0
venum <mgautam@protonmail.com>
2020-02-26 21:07:03 +0100

    Update app.js
    Now properly generating unique ID's

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const fs = require("fs");
 const port = 3000;
 const app = express();
 const idLength = 4;
+const pasteDir = "./pastes/";
 
 app.use(bodyParser.urlencoded({extended: true })); 
 app.use(express.static(__dirname + '/public'));
@@ -12,40 +13,38 @@ app.use(express.static(__dirname + '/public'));
 /*
 Handles post requests for saving pastes
 */
-app.post("/paste_publish", function(req, res) {
+app.post("/paste_publish", publishPaste);
+
+function publishPaste(req, res) {
     let id = generateID(idLength);
     let content = req.body.paste_content;
-    let file = "./pastes/" + id;
+    let file = pasteDir + id;
 
     fs.access(file, fs.constants.F_OK, (err => {
         if(err) {
-            break;
+            fs.appendFile(file, content, function (err) {
+                if (err) {
+                    console.log("An error occurred while creating/writing to paste file", err, file, content);
+                    res.writeHead(500, {"Content-Type" : "text/plain"});
+                    res.end("An unexpected server error occurred while saving your paste. Sorry ¯\_(ツ)_/¯")
+                }else {
+                    console.log("Paste " + id + " was created successfully. (at " + file + ")");
+                    res.writeHead(301, {"Location" : "/p/" + id});
+                    res.end();
+                }
+            });
         }else {
-            id = generateID(idLength);
-            content = req.body.paste_content;
-            file = "./pastes/" + id;
+            publishPaste(req, res);
         }
     }));
-
-    fs.appendFile(file, content, function (err) {
-        if (err) {
-            console.log("An error occurred while creating/writing to paste file", err, file, content);
-            res.writeHead(500, {"Content-Type" : "text/plain"});
-            res.end("An unexpected server error occurred while saving your paste. Sorry ¯\_(ツ)_/¯")
-        }else {
-            console.log("Paste " + id + " was created successfully. (at " + file + ")");
-            res.writeHead(301, {"Location" : "/p/" + id});
-            res.end();
-        }
-    });
-});
+}
 
 /*
 Inserts the text into the paste template
 */
 app.get("/p/*", function(req, res) {
     const id = req.url.slice(3);
-    fs.readFile("./pastes/" + id, function(error, pasteContent) {
+    fs.readFile(pasteDir + id, function(error, pasteContent) {
         if(error) {
             console.log(error);
             res.send("this paste does not seem to exist")
